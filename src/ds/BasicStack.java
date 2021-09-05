a0181939887ad57bbd8020a3fc63d28976d52166
MA068069 <mirshahin.akhtar@cerner.com>
2021-09-05 21:33:17 +0530

    ds stack

---
+++ src/ds/BasicStack.java
@@ -0,0 +1,49 @@
+package ds;
+
+public class BasicStack<X> {
+	private X [] data;
+	private int stackPointer;
+
+	public BasicStack() {
+		data = (X[])new Object[1000];
+		stackPointer = 0;
+	}
+
+	public void push (X newItem) {
+		data[stackPointer++] = newItem;
+	}
+	 
+	public X pop() {
+		if(stackPointer==0) {
+			throw new IllegalStateException ("no items in the stack");
+		}
+		return data[--stackPointer];
+		
+	}
+	
+	public boolean contains(X item) {
+		boolean found = false;
+		for (int i=0; i<stackPointer; i++ ) {
+			if(data[i].equals(item)) {
+				found = true;
+				break;
+			}
+		}
+		return found;
+	}
+	
+	public X access(X item) {
+		while (stackPointer > 0) {
+			X tmpItem = pop();
+			if(item.equals(tmpItem)) {
+				return tmpItem;
+			}
+		}
+		throw new IllegalArgumentException("Could not find the item:" + item);
+	}
+	
+	public int size() {
+	    return stackPointer;
+	    }
+	
+}
